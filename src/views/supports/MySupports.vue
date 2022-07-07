3e4e79d89dd2df466e52006306483ac66ad241e0
gabebritto <gabrielsouto16@gmail.com>
2022-07-07 11:20:53 -0300

    Supports Page Vue3

--- src/views/supports/MySupports.vue
+++ src/views/supports/MySupports.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="home">
-      Minhas Dúvidas
-  </div>
+    <div class="pageTitle">
+        <span class="title">Minhas Dúvidas</span>
+        <span class="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+    </div>
+    <div class="content">
+      <div class="container">
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li>Todos</li>
+                <li>Aguardando Minha Resposta</li>
+                <li>Aguardando Professor</li>
+                <li>Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+          <div class="right">
+              <div class="content">
+                <div class="comments">
+                    <supports-global/>
+                </div>
+              </div>
+          </div>
+      </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import SupportsGlobal from '@/components/Supports.vue'
 export default {
   name: 'MySupports',
+  components: {
+    SupportsGlobal
+  }
 }
 </script>
